149ee439a22dec1ab62bd9977136ae56f18a4451
= <george.bentes@gmail.com>
2023-03-10 22:57:31 -0300

    Add form to be the first one to be shown

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CreateThoughtComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
